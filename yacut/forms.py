8fcbaca9e5f3379397d2d87e5a9833651449292f
alex <alexmal24122004@gmail.com>
2024-09-26 16:49:57 +0300

    done unique_id func

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,19 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import URL, DataRequired, Length, Optional
 
 
 class LinkForm(FlaskForm):
     original_link = URLField(
         'Длинная ссылка',
         validators=[
-            DataRequired(message='Поле обязательно для ввода'),
+            DataRequired(message='Поле обязательно для ввода!'),
+            URL(require_tld=False, message='Введите правильный адрес!')
         ]
     )
     custom_id = StringField(
         'Ваш вариант короткой ссылки',
         validators=[
-            Length(6, 16, message='ID ссылки не в диапазоне от 6-16 символов'),
+            Length(6, 16, message='ID ссылки не в диапазоне от 6-16 символов!'),
             Optional(),
         ]
     )
--- yacut/views.py
+++ yacut/views.py
@@ -1,7 +1,16 @@
+import random
+
 from flask import render_template
 
 from . import app
 from .forms import LinkForm
+
+
+def get_unique_short_id():
+    choices = ([chr(i) for i in range(48, 58)] +
+               [chr(i) for i in range(65, 91)] +
+               [chr(i) for i in range(97, 123)])
+    return ''.join(random.choices(choices, k=6))
 
 
 @app.route('/', methods=['GET', 'POST'])
